95b4a2df46be1c89fb58df8156179c11d5e6c2da
Hemant <hemantksingh.hk@gmail.com>
2014-06-02 01:32:25 -0700

    Added a winforms chat room using eventstore.

---
+++ ChatRoom/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ChatRoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(124, 24);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(269, 120);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(197, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(12, 230);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 263);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ChatRoom/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ChatRoom
+{
+    public partial class Form1 : Form
+    {
+        private string _readToEnd;
+        private bool _stop;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static SyndicationLink
+            GetNamedLink(IEnumerable<SyndicationLink> links, string name)
+        {
+            return links.FirstOrDefault(link => link.RelationshipType == name);
+        }
+
+        private static Uri GetLast(Uri head)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(head);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/atom+xml";
+            try
+            {
+                using (var response = (HttpWebResponse) request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.NotFound)
+                        return null;
+                    using (XmlReader xmlreader =
+                        XmlReader.Create(response.GetResponseStream()))
+                    {
+                        SyndicationFeed feed = SyndicationFeed.Load(xmlreader);
+                        SyndicationLink last = GetNamedLink(feed.Links, "last");
+                        return (last != null)
+                            ? last.Uri
+                            : GetNamedLink(feed.Links, "self").Uri;
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                if (((HttpWebResponse) ex.Response).StatusCode ==
+                    HttpStatusCode.NotFound) return null;
+                throw;
+            }
+        }
+
+        private void ProcessItem(SyndicationItem item)
+        {
+            var action = new Action(() => textBox1.Text += (item.Title.Text) + Environment.NewLine);
+            BeginInvoke(action);
+
+            //get events
+            var request =
+                (HttpWebRequest) WebRequest.Create(GetNamedLink(item.Links,
+                    "alternate").Uri);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/json";
+            using (WebResponse response = request.GetResponse())
+            {
+                var streamReader = new
+                    StreamReader(response.GetResponseStream());
+                _readToEnd = streamReader.ReadToEnd();
+                var write = new Action(() => textBox1.Text += (_readToEnd) + Environment.NewLine);
+                BeginInvoke(write);
+            }
+        }
+
+        private Uri ReadPrevious(Uri uri)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(uri);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/atom+xml";
+            using (WebResponse response = request.GetResponse())
+            {
+                using (XmlReader xmlreader =
+                    XmlReader.Create(response.GetResponseStream()))
+                {
+                    SyndicationFeed feed = SyndicationFeed.Load(xmlreader);
+                    foreach (SyndicationItem item in feed.Items.Reverse())
+                    {
+                        ProcessItem(item);
+                    }
+                    SyndicationLink prev = GetNamedLink(feed.Links, "previous");
+                    return prev == null ? uri : prev.Uri;
+                }
+            }
+        }
+
+        private void PostMessage(string text)
+        {
+            string message =
+                string.Format(
+                    "[{{'eventType':'MyFirstEvent', 'eventId' :'" + Guid.NewGuid() +
+                    "','data' :{{'name':'{0}','number' :" + new Random().Next() + "}}}}]", text);
+
+            WebRequest request =
+                WebRequest.Create("http://127.0.0.1:2113/streams/chat-GeneralChat");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = message.Length;
+            using (var sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write(message);
+            }
+            using (WebResponse response = request.GetResponse())
+            {
+                response.Close();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _stop = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //var timer = new Timer(o => PostMessage(), null, 1000, 1000);
+
+            var backgroundWorker = new BackgroundWorker();
+
+            backgroundWorker.DoWork += (o, args) =>
+            {
+                Uri last = null;
+                while (last == null && !_stop)
+                {
+                    last = GetLast(new
+                        Uri("http://127.0.01:2113/streams/chat-GeneralChat"));
+                    if (last == null) Thread.Sleep(1000);
+                }
+
+                while (!_stop)
+                {
+                    Uri current = ReadPrevious(last);
+                    if (last == current)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                    last = current;
+                }
+            };
+
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PostMessage(textBox2.Text);
+            textBox2.Text = string.Empty;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart += textBox1.Text.Length;
+        }
+    }
+}
